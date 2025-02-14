a48bed97e5a05fd09b8351f0fb12f7d3ef6e9116
Walter Fan <walter.fan@zoom.us>
2025-02-14 13:30:43 +0800

    update note config

--- sticky_note.py
+++ sticky_note.py
@@ -1,8 +1,10 @@
+#!/usr/bin/env python3
+
 import tkinter as tk
 from tkinter import messagebox
 import os
-import sys
 import datetime
+import argparse
 
 # refer to https://pyyaml.org/wiki/PyYAMLDocumentation
 from yaml import load, dump
@@ -13,7 +15,6 @@ except ImportError:
     from yaml import Loader, Dumper
 
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
-DOC_DIR = os.path.join(os.path.dirname(CURRENT_DIR), "doc")
 
 class YamlConfig:
     def __init__(self, yaml_file):
@@ -49,8 +50,10 @@ class StickyNote:
 
         today = datetime.date.today()
         self.datestr = today.strftime("%Y-%m-%d")
-        self.default_folder = self.config.get_config_item("default", "folder")
+        self.default_folder = os.getenv("NOTE_FOLDER", self.config.get_config_item("default", "folder"))
         self.default_filename = f"{self.default_folder}/{self.datestr}.md"
+        self.auto_save_interval = int(self.config.get_config_item("default", "save_interval_ms"))
+        self.last_modified_time = None
     # Function to save a sticky note to a custom file
     def save_note(self, prompt=True):
         note_text = self.text_area.get("1.0", tk.END).strip()
@@ -69,9 +72,9 @@ class StickyNote:
         with open(f"{file_name}", "w") as file:
             file.write(f"Title: {title}\n\n{note_text}")
         if prompt:
-            messagebox.showinfo("Saved", f"Note saved successfully as {file_name}")
-        else:
-            pass
+            messagebox.showinfo("Saved", f"saved successfully of {file_name}")
+
+        self.last_modified_time = os.path.getmtime(file_name)
 
     # Function to load a sticky note from a file
     def load_note(self):
@@ -93,17 +96,34 @@ class StickyNote:
             messagebox.showwarning("File Not Found", f"No note found with the name {file_name}")
 
     def auto_save(self):
+        file_name = self.file_name_entry.get().strip()
+        if self.is_already_modified():
+            messagebox.showwarning("File changed", f"Something changed of {file_name}")
         self.save_note(False)  # Save the note
         self.root.after(5000, self.auto_save)
+
+    def is_already_modified(self):
+        file_name = self.file_name_entry.get().strip()
+        if os.path.exists(file_name):
+            current_modified_time = os.path.getmtime(file_name)
+            if self.last_modified_time and current_modified_time > self.last_modified_time:
+                self.last_modified_time = current_modified_time
+                return True
+            else:
+                return False
+        return False
 
     def initialize(self):
         # Create the main window
         self.root = tk.Tk()
         self.root.title(self.title)
 
+        self.root.grid_rowconfigure(2, weight=1)
+        self.root.grid_columnconfigure(0, weight=1)
+
         # Title input field (one line)
         self.title_frame = tk.Frame(self.root)
-        self.title_frame.pack(padx=10, pady=2)
+        self.title_frame.grid(row=0, column=0, padx=10, pady=2, sticky="ew")
 
         self.title_label = tk.Label(self.title_frame, text="Title:")
         self.title_label.grid(row=0, column=0, padx=5, pady=2, sticky="w")
@@ -113,7 +133,7 @@ class StickyNote:
 
         # File name input field (one line)
         self.file_name_frame = tk.Frame(self.root)
-        self.file_name_frame.pack(padx=10, pady=2)
+        self.file_name_frame.grid(row=1, column=0, padx=10, pady=2, sticky="ew")
 
         self.file_name_label = tk.Label(self.file_name_frame, text="File: ")
         self.file_name_label.grid(row=0, column=0, padx=5, pady=2, sticky="w")
@@ -121,13 +141,12 @@ class StickyNote:
         self.file_name_entry.grid(row=0, column=1, padx=5, pady=2)
         self.file_name_entry.insert(tk.END, self.default_filename)
 
-
-        # Text area to write notes (larger size)
+        # Text area to write notes (adjustable size)
         self.text_area = tk.Text(self.root, height=15, width=60, wrap=tk.WORD)
         self.scrollbar = tk.Scrollbar(self.root, command=self.text_area.yview)
         self.text_area.config(yscrollcommand=self.scrollbar.set)
-        self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
-        self.text_area.pack(padx=10, pady=5)
+        self.scrollbar.grid(row=2, column=1, sticky="ns")
+        self.text_area.grid(row=2, column=0, padx=10, pady=5, sticky="nsew")
         self.text_area.insert(tk.END, f"# {self.datestr}")
         self.text_area.focus_set()
 
@@ -138,7 +157,7 @@ class StickyNote:
 
         # Buttons for saving and loading notes
         self.button_frame = tk.Frame(self.root)
-        self.button_frame.pack(padx=10, pady=5)
+        self.button_frame.grid(row=3, column=0, padx=10, pady=5, sticky="ew")
 
         save_button = tk.Button(self.button_frame, text="Save Note", command=self.save_note)
         save_button.grid(row=0, column=0, padx=5)
@@ -156,10 +175,13 @@ class StickyNote:
         return self.root
 
 if __name__ == "__main__":
-    yaml_file = os.path.join(CURRENT_DIR, "sticky_note.yaml")
+    parser = argparse.ArgumentParser(description="Daily Sticky Note Application")
+    parser.add_argument('-f', '--file', type=str, default=os.path.join(CURRENT_DIR, "sticky_note.yaml"), help='Path to the YAML configuration file')
+    args = parser.parse_args()
+
+    yaml_file = args.file
     yaml_config = YamlConfig(yaml_file)
 
     app = StickyNote(yaml_config)
     app.initialize()
-
 
